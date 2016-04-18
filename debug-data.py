30b33b2a414fb8a879911cd7c09557f7db13076c
Andy Kitchen <kitchen.andy@gmail.com>
2016-04-18 14:53:31 +1000

    texture debug polygons

--- debug-data.py
+++ debug-data.py
@@ -1,10 +1,21 @@
 import numpy as np
-from skimage.draw import polygon
+import skimage
 
 def random_polygon(w=10, h=10, sides=5):
     img = np.zeros((w, h), dtype=np.uint8)
     x = np.random.randint(0, w, size=sides)
     y = np.random.randint(0, h, size=sides)
-    rr, cc = polygon(y, x)
+    rr, cc = skimage.draw.polygon(y, x)
     img[rr, cc] = 1
     return img
+
+grass_image = skimage.io.imread('grass.jpg')
+stones_image = skimage.io.imread('stones.jpg')
+
+def random_textured_polygon(w=256, h=256, sides=5):
+	stones_small = skimage.transform.resize(stones_image[:,:stones_image.shape[0]], (h, w))
+	grass_small = skimage.transform.resize(grass_image[:,:grass_image.shape[0]], (h, w))
+
+	mask = random_polygon(w, h, 5)[:,:,np.newaxis]
+	img = mask*grass_small + (1 - mask)*stones_small
+	return img
