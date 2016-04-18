f50433530c021b34e60a7478aee36ce67c9f6c80
Andy Kitchen <kitchen.andy@gmail.com>
2016-04-18 14:58:07 +1000

    return mask with textured polygon

--- debug-data.py
+++ debug-data.py
@@ -2,7 +2,7 @@ import numpy as np
 import skimage
 
 def random_polygon(w=10, h=10, sides=5):
-    img = np.zeros((w, h), dtype=np.uint8)
+    img = np.zeros((h, w), dtype=np.uint8)
     x = np.random.randint(0, w, size=sides)
     y = np.random.randint(0, h, size=sides)
     rr, cc = skimage.draw.polygon(y, x)
@@ -16,6 +16,8 @@ def random_textured_polygon(w=256, h=256, sides=5):
 	stones_small = skimage.transform.resize(stones_image[:,:stones_image.shape[0]], (h, w))
 	grass_small = skimage.transform.resize(grass_image[:,:grass_image.shape[0]], (h, w))
 
-	mask = random_polygon(w, h, 5)[:,:,np.newaxis]
-	img = mask*grass_small + (1 - mask)*stones_small
-	return img
+	mask = random_polygon(w, h, 5)
+
+	mask3 = mask[:,:,np.newaxis]
+	img = mask3*grass_small + (1 - mask3)*stones_small
+	return img, mask
