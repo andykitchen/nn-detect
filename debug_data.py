f57bf78f1324c3a3c956116222f3dff4c4080012
Andy Kitchen <kitchen.andy@gmail.com>
2016-04-26 12:27:51 +1000

    many improvements:
    - initialize model with smaller weights
    - added 'loss' scope
    - added more data generation functions

--- debug_data.py
+++ debug_data.py
@@ -6,7 +6,7 @@ import skimage.transform
 import skimage.morphology
 import skimage.color
 
-def random_polygon(w=10, h=10, sides=5):
+def random_polygon(w=128, h=128, sides=5):
     img = np.zeros((h, w), dtype=np.uint8)
     x = np.random.randint(0, w, size=sides)
     y = np.random.randint(0, h, size=sides)
@@ -14,29 +14,59 @@ def random_polygon(w=10, h=10, sides=5):
     img[rr, cc] = 1
     return img
 
-grass_image = skimage.io.imread('grass.jpg')
-stones_image = skimage.io.imread('stones.jpg')
 
-def random_textured_polygon(w=256, h=256, sides=5):
-	stones_small = skimage.transform.resize(stones_image[:,:stones_image.shape[0]], (h, w))
-	grass_small = skimage.transform.resize(grass_image[:,:grass_image.shape[0]], (h, w))
-
+def random_textured_polygon(fg, bg, w=128, h=128, sides=5):
 	mask = random_polygon(w, h, 5)
 	chull = skimage.morphology.convex_hull_image(mask)
 	mask[chull] = 1
 
 	mask = mask[:,:,np.newaxis]
-	img = mask*grass_small + (1 - mask)*stones_small
+	img = mask*fg + (1 - mask)*bg
 	return img, mask
 
-def generate_batch(w=128, h=128, minibatch_size=10, sides=5):
-	input_data = np.zeros([minibatch_size, h, w, 1])
+
+def load_default_textures(w, h):
+	grass_image  = skimage.io.imread('grass.jpg')
+	stones_image = skimage.io.imread('stones.jpg')
+
+	stones_small = skimage.transform.resize(stones_image[:,:stones_image.shape[0]], (h, w))
+	grass_small  = skimage.transform.resize(grass_image[:,:grass_image.shape[0]], (h, w))
+
+	return grass_small, stones_small
+
+
+def generate_easy_batch(w=128, h=128, minibatch_size=10, sides=5):
+	input_data = np.zeros([minibatch_size, h, w, 1], dtype=np.float32)
 	label_data = np.zeros([minibatch_size, h, w, 1], dtype=np.int64)
 
 	for i in range(minibatch_size):
-		img, mask = random_textured_polygon(w, h, sides)
+		mask = random_polygon(w, h, sides)
+		input_data[i, :, :, 0] = 2.*mask - 1.
+		label_data[i, :, :, 0] = mask
+
+	return input_data, label_data
+
+
+def generate_color_batch(fg, bg, w=128, h=128, minibatch_size=10, sides=5):
+	input_data = np.zeros([minibatch_size, h, w, 3], dtype=np.float32)
+	label_data = np.zeros([minibatch_size, h, w, 1], dtype=np.int64)
+
+	for i in range(minibatch_size):
+		img, mask = random_textured_polygon(fg, bg, w, h, sides)
+		input_data[i, :, :] = 2.*img - 1.
+		label_data[i, :, :] = mask
+
+	return input_data, label_data
+
+
+def generate_grayscale_batch(fg, bg, w=128, h=128, minibatch_size=10, sides=5):
+	input_data = np.zeros([minibatch_size, h, w, 1], dtype=np.float32)
+	label_data = np.zeros([minibatch_size, h, w, 1], dtype=np.int64)
+
+	for i in range(minibatch_size):
+		img, mask = random_textured_polygon(fg, bg, w, h, sides)
 		img_gray = skimage.color.rgb2gray(img)
-		input_data[i, :, :, 0] = 2*(img_gray / 255) - 1
+		input_data[i, :, :, 0] = 2.*img_gray - 1.
 		label_data[i, :, :] = mask
 
 	return input_data, label_data
--- model.py
+++ model.py
@@ -6,64 +6,89 @@ import tensorflow as tf
 minibatch_size = 16
 input_size = 128
 input_channels = 1
+# input_channels = 3
 
 conv1_size = 5
 conv1_stride = 1
-conv1_features = 8
+conv1_features = 16
 
-output_conv_size = 1
+output_conv_size = 5
 output_stride = 1
 output_features = 2
 
-num_iterations = 10
+num_iterations = 1000
+learning_rate = 0.1
+report_frequency = 10
+random_init_stddev = 1e-4
 
-# if checkpoint exists, load from checkpoint
+flat_size = minibatch_size * input_size * input_size
 
-np.random.seed(42)
+random_seed = 42
+
+
+np.random.seed(random_seed)
+tf.set_random_seed(random_seed)
 
 with tf.Session() as sess:
 	inputs = tf.placeholder(tf.float32, shape=[minibatch_size, input_size, input_size, input_channels], name='inputs')
 	labels = tf.placeholder(tf.int64, shape=[minibatch_size, input_size, input_size, 1], name='labels')
 
 	with tf.name_scope('conv1') as scope:
-		conv1_weights  = tf.Variable(0.01*np.random.randn(conv1_size, conv1_size, input_channels, conv1_features).astype(np.float32), name='weights')
-		conv1_bias     = tf.Variable(np.zeros([conv1_features], dtype=np.float32), name='bias')
+		conv1_init     = tf.truncated_normal([conv1_size, conv1_size, input_channels, conv1_features], stddev=random_init_stddev, dtype=tf.float32)
+		conv1_weights  = tf.Variable(conv1_init, name='weights')
+		conv1_bias     = tf.Variable(tf.zeros([conv1_features], dtype=np.float32), name='bias')
 
 		conv1_y        = tf.nn.conv2d(inputs, conv1_weights, [1, conv1_stride, conv1_stride, 1], padding='SAME', name='y')
-		conv1_activity = tf.nn.relu(conv1_y + conv1_bias, name='activity')
+		conv1_bias     = tf.nn.bias_add(conv1_y, conv1_bias)
+		conv1_activity = tf.nn.relu(conv1_bias , name='activity')
 
 	with tf.name_scope('output') as scope:
-		output_weights     = tf.Variable(0.01*np.random.randn(output_conv_size, output_conv_size, conv1_features, output_features).astype(np.float32))
-		output_bias        = tf.Variable(np.zeros([output_features], dtype=np.float32))
+		output_init    = tf.truncated_normal([output_conv_size, output_conv_size, conv1_features, output_features], stddev=random_init_stddev, dtype=tf.float32)
+		output_weights = tf.Variable(output_init, name='weights')
+		output_bias    = tf.Variable(tf.zeros([output_features], dtype=np.float32), name='bias')
 
-		output_y           = tf.nn.conv2d(conv1_activity, output_weights, [1, output_stride, output_stride, 1], padding='SAME')
-		output_logits      = output_y + output_bias
-		output_probability = tf.sigmoid(output_logits)
+		output_y       = tf.nn.conv2d(conv1_activity, output_weights, [1, output_stride, output_stride, 1], padding='SAME', name='y')
+		output_logits  = output_y + output_bias
+
+		output_logits_flat = tf.reshape(output_logits, [flat_size, output_features])
+		output_pr_flat = tf.nn.softmax(output_logits_flat)
+		output_pr      = tf.reshape(output_pr_flat, [minibatch_size, input_size, input_size, output_features])
 
 	with tf.name_scope('loss') as scope:
-		flat_size = minibatch_size * input_size * input_size
-
 		output_logits_flat = tf.reshape(output_logits, [flat_size, output_features])
 		labels_flat = tf.reshape(labels, [flat_size])
 
-		loss_raw = tf.nn.sparse_softmax_cross_entropy_with_logits(output_logits_flat, labels_flat)
+		loss_raw = tf.nn.sparse_softmax_cross_entropy_with_logits(output_logits_flat, labels_flat, name='xentropy')
 		loss = tf.reduce_mean(loss_raw)
+		tf.scalar_summary("loss", loss)
+
 
 	trainable_vars = tf.trainable_variables()
 
-	opt = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-	opt_op = opt.minimize(loss, var_list=trainable_vars)
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+	train_op = optimizer.minimize(loss, var_list=trainable_vars)
 
 	writer = tf.train.SummaryWriter("/tmp/nn-detect-log",
-	                                sess.graph.as_graph_def(add_shapes=True))
+									sess.graph.as_graph_def(add_shapes=True))
 
-	init_op = tf.initialize_all_variables()
-	init_op.run()
+	tf.initialize_all_variables().run()
+
+	summary_op = tf.merge_all_summaries()
+
+	fg, bg = debug_data.load_default_textures(input_size, input_size)
 
 	for i in range(num_iterations):
-		input_data, label_data = debug_data.generate_batch(w=input_size, h=input_size, minibatch_size=minibatch_size)
-		_, loss_val = sess.run([opt_op, loss], feed_dict={inputs: input_data, labels: label_data})
-		print "current loss: ", loss_val
+		# input_data, label_data = debug_data.generate_easy_batch(w=input_size, h=input_size, minibatch_size=minibatch_size)
+		# input_data, label_data = debug_data.generate_color_batch(fg, bg, w=input_size, h=input_size, minibatch_size=minibatch_size)
+		input_data, label_data = debug_data.generate_grayscale_batch(fg, bg, w=input_size, h=input_size, minibatch_size=minibatch_size)
+		feed = {inputs: input_data, labels: label_data}
+		_, summary_value, loss_value = sess.run([train_op, summary_op, loss], feed_dict=feed)
+
+		writer.add_summary(summary_value, i)
+
+		if i % report_frequency == 0:
+			print "iteration:", i, "loss:", loss_value
+
 		# ...save current loss
 		# if 50 iterations have happened
 		#    save checkpoint
