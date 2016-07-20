4292674fe93f02a1948b9068cad0df8f863bbea9
Andy Gelme <andyg@geekscape.org>
2016-07-20 19:13:07 +1000

    Added "rr_video.py" UDP output and NodeJS UDP input example.

--- rr_video.py
+++ rr_video.py
@@ -1,9 +1,13 @@
+import cv2
 import numpy as np
 import tensorflow as tf
-
-import cv2
+import socket
 
 import rr_graph
+
+udp_host = "localhost"
+udp_port = 4000
+sock     = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 key_wait_time = 10
 input_size = 128
@@ -21,25 +25,27 @@ saver = tf.train.Saver()
 saver.restore(sess, '/tmp/rr-detect-log/checkpoint-1000')
 
 def process_frame(frame):
-	feed = {inputs: frame[np.newaxis, :, :, np.newaxis]}
-	output_value = sess.run(output, feed_dict = feed)
-	return output_value
+  feed = {inputs: frame[np.newaxis, :, :, np.newaxis]}
+  output_value = sess.run(output, feed_dict = feed)
+  return output_value[0]
 
 while capture.isOpened():
-	success, frame = capture.read()
+  success, frame = capture.read()
 
-	if success:
-		frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		frame_small = cv2.resize(frame_gray, (128, 128))
-		cv2.imshow('video', frame_small)
+  if success:
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_small = cv2.resize(frame_gray, (128, 128))
+    cv2.imshow('video', frame_small)
 
-		print process_frame(frame_small)
+    output_value =  process_frame(frame_small)
+    print output_value
+    sock.sendto(output_value.astype('|S6'), (udp_host, udp_port))
 
-		ch = cv2.waitKey(key_wait_time) & 0xFF
-		if ch == 27:
-			break
-		if ch == ord('q'):
-			break
+    ch = cv2.waitKey(key_wait_time) & 0xFF
+    if ch == 27:
+      break
+    if ch == ord('q'):
+      break
 
 capture.release()
 cv2.destroyAllWindows()
