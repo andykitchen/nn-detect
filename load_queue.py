cd89f247a21d3dec85764baec5acfe4ae860281c
Andy Kitchen <kitchen.andy+git@gmail.com>
2016-07-20 19:16:50 +1000

    updated queue loading

--- load_queue.py
+++ load_queue.py
@@ -4,15 +4,21 @@ import tensorflow as tf
 import os
 
 image_files = [os.path.join(root, file) \
-    for root, dirs, files in os.walk("resources") \
+    for root, dirs, files in os.walk("images") \
         for file in files \
-            if file.endswith(".jpeg") or file.endswith(".jpg")] 
+            if file.startswith("image_")] 
 
-image_file_queue = tf.train.string_input_producer(image_files, num_epochs=3, shuffle=True)
-dequeue_image_file = image_file_queue.dequeue()
+mask_files = [file.replace("image_", "mask_") for file in image_files]
+
+q = tf.FIFOQueue(capacity=3, dtypes=[tf.string, tf.string])
+enq = q.enqueue_many([image_files, mask_files])
+qr = tf.train.QueueRunner(q, [enq])
+tf.train.add_queue_runner(qr)
+
+image_path, mask_path = q.dequeue()
 
 reader = tf.WholeFileReader()
-image_file_name, image_data = reader.read(image_file_queue)
+image_file_name, image_data = reader.read(q)
 read_image = tf.image.decode_jpeg(image_data)
 
 init_op = tf.initialize_all_variables()
@@ -25,8 +31,8 @@ with tf.Session() as sess:
 
     try:
         while not coord.should_stop():
-            name, im = sess.run([image_file_name, read_image])
-            print name, im.shape
+            n1, n2 = sess.run([image_path, mask_path])
+            print n1, n2
 
     except tf.errors.OutOfRangeError:
         print('Done training -- epoch limit reached')
